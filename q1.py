17eef491de88c278e4ee4fe7ecfea236b548d851
Ashly Lau <ashlylau@Ashlys-MacBook-Pro.local>
2018-10-25 19:43:16 +0100

    completed question 1

--- q1.py
+++ q1.py
@@ -2,8 +2,14 @@
 
 import numpy as np
 
-# modify this function, and create other functions below as you wish
-def question01(portfolios):
-  # modify and then return the variable below
-  answer = 0
-  return answer
+def question01(portfolios): # input is an array of portfolios represented as their decimal values [p1,...,pN] where N<=100
+                            # each has a maximum value of 2^16-1
+  max = 0
+  for i in range(len(portfolios)):
+      p = portfolios[i]
+      for j in range(i+1, len(portfolios)):
+          q = portfolios[j]
+          if p ^ q > max:
+              max = p ^ q
+
+  return max
